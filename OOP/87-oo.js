dc188ddca8674e7ddbf6d66cb7e2828af812b9c5
Daniel Silva <dani3l.dxp@gmail.com>
2019-12-13 11:29:15 -0300

    about: Formas de criação de objetos

--- OOP/87-oo.js
+++ OOP/87-oo.js
@@ -23,3 +23,42 @@ const carro = {
 }
 
 console.log(carro)
+
+//* Formas de se criar Objetos
+
+// 1 - Literal
+
+const obj1 = {}
+
+// 2 - Nativa
+
+const obj2 = new Object()
+
+// 3 - Funções construtoras
+
+function Produto(nome, preco, desc) {
+    this.nome = nome
+    this.getPrecoComDesconto = () => {
+        return preco * (1 - desc)
+    }
+}
+
+const prod1 = new Produto('Café', 12.69, 0.15)
+
+console.log(prod1.getPrecoComDesconto())
+
+// 4 - Função Factory
+
+function Funcionario(nome, salarioBase, faltas) {
+    return {
+        nome,
+        salarioBase,
+        faltas,
+        getSalario() {
+            return (salarioBase / 30) * (30 - faltas)
+        }
+    }
+}
+
+const f1 = Funcionario('Daniel', 6500, 2)
+console.log(f1.getSalario())
