f538878ec88be4a5ec4d46367c3a8292d1c5df38
Daniel Silva <dani3l.dxp@gmail.com>
2019-12-17 17:37:10 -0300

    about: Conhecendo métodos importantes de array

---
+++ Array/105-arrayMetodos.js
@@ -0,0 +1,26 @@
+const pilotos = ['Vettel', 'Alonso', 'Raikkonen', 'Massa']
+
+pilotos.push('Verstappen') // Adiciona valores ao final do array - FILA
+console.log(`Push: ${pilotos}`)
+pilotos.pop() // Remove o ultimo valor do array - FILA
+console.log(`Pop: ${pilotos}`)
+pilotos.unshift('Hamilton') // Adiciona valores ao inicio do array - PILHA
+console.log(`Unshift: ${pilotos}`)
+pilotos.shift() // Remove o primeiro valor do array - PILHA
+console.log(`Shift: ${pilotos}`)
+
+//Add
+pilotos.splice(2, 0, 'Bottas', 'Massa')
+console.log(`Splice Add: ${pilotos}`)
+
+//Remove
+pilotos.splice(3, 1)
+console.log(`Splice remove: ${pilotos}`)
+
+//Métodos que retornam novo array
+
+const algunsPilotos = pilotos.slice(2) //Retorna um novo array à partir do indice
+console.log(algunsPilotos)
+
+const algunsPilotos2 = pilotos.slice(1, 4)
+console.log(algunsPilotos2)
