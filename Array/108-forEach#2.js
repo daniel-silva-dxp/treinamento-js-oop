3824f432b309023ce8b801be1bd2c1237b07186a
Daniel Silva <dani3l.dxp@gmail.com>
2019-12-17 19:04:57 -0300

    about: Implementando o comportamento do método forEach()

---
+++ Array/108-forEach#2.js
@@ -0,0 +1,15 @@
+// Implementando o comportamento do método forEach()
+Array.prototype.forEachs = function(callback) {
+    for(let i = 0; i < this.length; i++) {
+        callback(this[i], i, this)
+    }
+}
+
+const aprovados = ['Daneil', 'Matheus', 'Flávia', 'Fabiana']
+
+aprovados.forEachs(function(element, index){
+    aprovados.sort()
+    console.log(`${index + 1}- ${element}`)
+})
+
+
