1fe30cdb5e164a050b6d3087ca03f73425383ac6
Daniel Silva <dani3l.dxp@gmail.com>
2019-12-17 17:55:09 -0300

    about: Entendendo o forEach()

---
+++ Array/107-forEach#1.js
@@ -0,0 +1,13 @@
+const aprovados = ['Daneil', 'Matheus', 'Flávia', 'Fabiana']
+
+aprovados.forEach(function(element, index){
+    aprovados.sort()
+    console.log(`${index + 1}- ${element}`)
+})
+
+//OU
+
+aprovados.forEach(element => console.log(element))
+
+//OU
+aprovados.forEach((element, index, array) => console.log(`${index + 1}) ${element} no array: ${array}`))
