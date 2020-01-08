9fa470ef1598293f72a6d1fda2a21d9be2525047
Daniel Silva <dani3l.dxp@gmail.com>
2020-01-08 19:19:14 -0300

    about: Desafio para converter JSON em Object e retornar parte do objeto com map()

---
+++ Array/110-map#2.js
@@ -0,0 +1,13 @@
+const carrinho = [
+    '{ "nome": "Borracha", "preco": 3.45 }',
+    '{ "nome": "Caderno", "preco": 13.90 }',
+    '{ "nome": "Kit de Lapis", "preco": 41.22 }',
+    '{ "nome": "Caneta", "preco": 7.50 }'
+]
+
+const result = carrinho.map((e) => {
+    let element = JSON.parse(e)
+    return element.preco
+})
+
+console.log(result)
