4b9405d31ef652a9dec4ba70db74c31a9c012d60
Daniel Silva <dani3l.dxp@gmail.com>
2020-01-17 15:25:08 -0300

    about: Arguments - parâmetros variáveis

--- DevJS/Funcoes/172-funcoes.js
+++ DevJS/Funcoes/172-funcoes.js
@@ -3,3 +3,21 @@ function soma(a, b) {
 }
 
 console.log(soma(10, 15));
+
+// Parâmetros variáveis
+function arr() {
+	return [ ...arguments ];
+}
+
+console.log(arr(7, 5, 3.2));
+
+function sum() {
+	let result = 0;
+	for (let i in arguments) {
+		result += arguments[i];
+	}
+
+	return result;
+}
+
+console.log(sum(1, 10, 25, -76.25));
