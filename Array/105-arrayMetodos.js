68e0e6be481ba69823f43f435d46e4bf364796a7
Daniel Silva <dani3l.dxp@gmail.com>
2020-01-17 15:24:25 -0300

    about: Método sort

--- Array/105-arrayMetodos.js
+++ Array/105-arrayMetodos.js
@@ -18,9 +18,17 @@ pilotos.splice(3, 1);
 console.log(`Splice remove: ${pilotos}`);
 
 //Métodos que retornam novo array
-
 const algunsPilotos = pilotos.slice(2); //Retorna um novo array à partir do indice
 console.log(algunsPilotos);
 
 const algunsPilotos2 = pilotos.slice(1, 4);
 console.log(algunsPilotos2);
+
+// Ordenando array
+const pessoas = [ 'Fabiana', 'Flávia', 'Matheus', 'Daniel' ];
+const nums = [ 12, 40, 3, 7, 19, 1 ];
+
+console.log(`Padrão: ${pessoas}`);
+console.log(`Ordenados: ${pessoas.sort()}`);
+console.log(`Padrão: ${nums}`);
+console.log(`Ordenados: ${nums.sort((a, b) => a - b)}`);
