3ae3a02b2885ec9bb3ed379114d9adc85426bc2d
Daniel Silva <dani3l.dxp@gmail.com>
2020-03-02 13:06:26 -0300

    feat: exercício de elevação matemática

---
+++ Exercises/03.js
@@ -0,0 +1,7 @@
+// 03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+function foo(b, e) {
+	return Math.pow(b, e); // OU
+	// return b ** e
+}
+
+console.log(foo(2, 3));
