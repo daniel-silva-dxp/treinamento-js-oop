87c0f76f24c33bb7af97132978f04cc8b127c2e3
Daniel Silva <dani3l.dxp@gmail.com>
2020-03-13 10:14:25 -0300

    feat: treinando o método reduce()

--- Array/114-reduce.js
+++ Array/114-reduce.js
@@ -11,3 +11,39 @@ const result = alunos.map((a) => a.nota).reduce((acumulador, atual) => {
 	console.log(acumulador, atual);
 	return acumulador + atual;
 });
+
+// reduce() de um array de arrays
+const arr = [ [ 0, 1 ], [ 2, 3 ], [ 4 ], [ 5 ] ];
+const arrConcat = arr.reduce((acumulador, atual) => acumulador.concat(atual));
+
+console.log(arr);
+console.log(arrConcat);
+
+// Contando valores iguais em um objeto
+const names = [ 'Daniel', 'Matheus', 'Flávia', 'Daniel', 'Daniel', 'Flávia' ];
+const contedNames = names.reduce((acumul, current) => {
+	if (current in acumul) {
+		acumul[current]++;
+	} else {
+		acumul[current] = 1;
+	}
+	return acumul;
+}, {});
+
+console.log(contedNames);
+
+// Agrupando objetos por uma propriedade
+const pessoas = [ { nome: 'Alice', idade: 21 }, { nome: 'Max', idade: 20 }, { nome: 'Jane', idade: 20 } ];
+
+function groupFor(objArr, propertie) {
+	return objArr.reduce((acc, obj) => {
+		let key = obj[propertie];
+		if (!acc[key]) {
+			acc[key] = [];
+		}
+		acc[key].push(obj);
+		return acc;
+	}, {});
+}
+
+console.log(groupFor(pessoas, 'idade'));
